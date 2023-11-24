983dd2fd8e5650b115394233a0f1a19a1382265b
cristicris6 <cris_s_2019@yahoo.com>
2023-11-24 11:39:53 +0200

    Update Teleop.java

--- src/main/java/org/firstinspires/ftc/teamcode/Hardware/Teleop.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Hardware/Teleop.java
@@ -4,27 +4,39 @@ import android.graphics.ColorSpace;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.Hardware.Depositor.LiftStates;
 import org.firstinspires.ftc.teamcode.Hardware.Intake.IntakeStates;
+import org.firstinspires.ftc.teamcode.Hardware.Intake.IntakeSpinnerStates
 
 
 
 public class TeleOp{
-    boolean currentGamepad;
-    boolean previousGamepad;
-    public void(Gamepad Gamepad    LiftStates LiftStates) {
-        boolean currentGamepad;
-        boolean previousGamepad;
+
+    public void(Gamepad currentgamepad Gamepad previousgamepad   LiftStates LiftStates) {
+        boolean currentgamepad;
+        boolean previousgamepad;
         LiftStates = LiftStates.RETRACT;
-        if (!currentGamepad.dpad_up && previousGamepad.dpad_up) {
+        if (currentgamepad.dpad_up && !previousgamepad.dpad_up) {
             LiftStates = (LiftStates + 1) % 11;
 
 
         }
     }
-        public void(IntakeStates IntakeStates Gamepad Gamepad){
-        if(!currentGamepad.a && previousGamepad.a){
-            Intake.IntakeStates.COLLECT = Intake.IntakeStates.TRANSFER
+        public void(Gamepad gamepad IntakeStates IntakeStates ) {
+            boolean currentgamepad;
+            boolean previousgamepad;
+            if (!currentgamepad.a && !previousgamepad.a) {
+                Intake.IntakeStates.COLLECT = Intake.IntakeStates.TRANSFER
+            }
         }
+
+        public void(Gamepad gamepad IntakeSpinnerStates IntakeSpinnerStates ){
+        if(gamepad.right_trigger){
+            IntakeSpinnerStates.COLLECT;
         }
+        else if(gamepad.left_trigger){
+            IntakeSpinnerStates.REVERSE
+        }
+        else IntakeSpinnerStates.STOP
+
 
 
         }
